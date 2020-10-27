0ab751c564c37f52f13964200d3baf9a23588b00
com <123@.com>
2020-10-27 23:17:30 +0530

    virtual env

--- socialmedia/asgi.py
+++
@@ -1,6 +0,0 @@
-import django
-import os
-from channels.routing import get_default_application
-os.environ.setdefault("DJANGO_SETTING_MODULE","socialmedia.settings")
-django.setup()
-application = get_default_application()
